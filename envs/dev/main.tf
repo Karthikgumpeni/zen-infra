8b8df4fefb2030f6c258a43bcc5c92e208d9f1c3
Karthikgumpeni <karthikgumpeni@gmail.com>
2026-09-01 16:31:34 +0530

    Change the Instance type

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -29,10 +29,10 @@ module "eks" {
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.private_subnets
   kubernetes_version = "1.33"
-  instance_types     = ["t2.medium"]
-  min_size           = 1
-  max_size           = 2
-  desired_size       = 1
+  instance_types = ["t3.small"]
+  min_size       = 1
+  max_size       = 3
+  desired_size   = 3
 }
 
 module "rds" {
