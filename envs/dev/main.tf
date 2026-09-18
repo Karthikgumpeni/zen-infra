82544e40c9821c4cc5444b37979d771dfb128aba
Karthikgumpeni <karthikgumpeni@gmail.com>
2026-09-18 20:21:50 +0530

    adding extra nodes

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -31,8 +31,8 @@ module "eks" {
   kubernetes_version = "1.33"
   instance_types = ["t3.small"]
   min_size       = 1
-  max_size       = 5
-  desired_size   = 4
+  max_size       = 10
+  desired_size   = 8
 }
 
 module "rds" {
